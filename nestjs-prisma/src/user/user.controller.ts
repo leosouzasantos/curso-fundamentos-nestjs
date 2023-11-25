495cc1252f76c615863d37b803ea9bf374074d73
Leo Souza <leonardodomaria@gmail.com>
2023-11-25 17:45:54 -0300

    aulas sobre prisma ORM

--- nestjs-prisma/src/user/user.controller.ts
+++ nestjs-prisma/src/user/user.controller.ts
@@ -12,56 +12,45 @@ import {
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
-  async create(@Body() { email, name, password }: CreateUserDTO) {
-    return { email, name, password };
+  async create(@Body() data: CreateUserDTO) {
+    return this.userService.create(data);
   }
 
   @Get()
   async list() {
-    return { users: [] };
+    return this.userService.list();
   }
 
   @Get(':id')
-  async show(@Param('id', ParseIntPipe) param) {
-    return { user: {}, param };
+  async show(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.show(id);
   }
 
   @Put(':id')
   async update(
-    @Body() { email, name, password }: UpdatePutUserDTO,
-    @Param('id', ParseIntPipe) param,
+    @Body() data: UpdatePutUserDTO,
+    @Param('id', ParseIntPipe) id: number,
   ) {
-    return {
-      medthod: 'put',
-      email,
-      name,
-      password,
-      param,
-    };
+    return this.userService.update(id, data);
   }
 
   @Patch(':id')
   async updatePartial(
-    @Body() { email, name, password }: UpdatePatchUserDTO,
-    @Param('id', ParseIntPipe) params,
+    @Body() data: UpdatePatchUserDTO,
+    @Param('id', ParseIntPipe) id: number,
   ) {
-    return {
-      method: 'patch',
-      email,
-      name,
-      password,
-      params,
-    };
+    return this.userService.updatePartial(id, data);
   }
 
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
-    return {
-      id,
-    };
+    return this.userService.delete(id);
   }
 }
