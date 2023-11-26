fcd1bb49ee168bf7813c1d5f8041a2fd2b6bd680
Leo Souza <leonardodomaria@gmail.com>
2023-11-26 17:13:32 -0300

    aulas sobre conceitos fundamentais

--- nestjs-prisma/src/user/user.controller.ts
+++ nestjs-prisma/src/user/user.controller.ts
@@ -3,8 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  Param,
-  ParseIntPipe,
   Patch,
   Post,
   Put,
@@ -13,6 +11,7 @@ import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UserService } from './user.service';
+import { ParamId } from 'src/decorators/param-id.decorator';
 
 @Controller('users')
 export class UserController {
@@ -29,28 +28,23 @@ export class UserController {
   }
 
   @Get(':id')
-  async show(@Param('id', ParseIntPipe) id: number) {
+  async show(@ParamId() id: number) {
+    console.log('ID: ', id);
     return this.userService.show(id);
   }
 
   @Put(':id')
-  async update(
-    @Body() data: UpdatePutUserDTO,
-    @Param('id', ParseIntPipe) id: number,
-  ) {
+  async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number) {
     return this.userService.update(id, data);
   }
 
   @Patch(':id')
-  async updatePartial(
-    @Body() data: UpdatePatchUserDTO,
-    @Param('id', ParseIntPipe) id: number,
-  ) {
+  async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id: number) {
     return this.userService.updatePartial(id, data);
   }
 
   @Delete(':id')
-  async delete(@Param('id', ParseIntPipe) id: number) {
+  async delete(@ParamId() id: number) {
     return this.userService.delete(id);
   }
 }
--- nestjs-prisma/src/user/user.service.ts
+++ nestjs-prisma/src/user/user.service.ts
@@ -19,6 +19,8 @@ export class UserService {
   }
 
   async show(id: number) {
+    await this.exists(id);
+
     return this.prisma.user.findUnique({
       where: {
         id,
@@ -91,7 +93,13 @@ export class UserService {
   }
 
   async exists(id: number) {
-    if (!(await this.show(id))) {
+    if (
+      !(await this.prisma.user.count({
+        where: {
+          id,
+        },
+      }))
+    ) {
       throw new NotFoundException(`O usuário ${id} não existe.`);
     }
   }
